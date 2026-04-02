155e7ef4a8aec58a077f7c93019cd8c1106c9617
Fin Prindle <24947666+Fin-bar@users.noreply.github.com>
2026-04-01 19:45:57 -0700

    Added a second lctl to gaming maps for schmoovement; moved media play/pause to flipped placement

--- keyboards/zsa/moonlander/keymaps/fin/keymap.c
+++ keyboards/zsa/moonlander/keymaps/fin/keymap.c
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRAVE,       KC_QUOTE,       KC_COMMA,       KC_DOT,         KC_P,           KC_Y,           KC_F1,                                          KC_F4,          KC_F,           KC_G,           KC_C,           KC_R,           KC_L,           KC_BSLS,
         KC_TAB,         KC_A,           KC_O,           KC_E,           KC_U,           KC_I,           KC_SCLN,                                        KC_MINS,        KC_D,           KC_H,           KC_T,           KC_N,           KC_S,           KC_EQL,
         KC_LSFT,        KC_SLASH,       KC_Q,           KC_J,           KC_K,           KC_X,                                                                           KC_B,           KC_M,           KC_W,           KC_V,           KC_Z,           KC_LSFT,
-        KC_MPLY,        KC_LBRC,        KC_RBRC,        KC_LALT,        KC_LCTL,                        KC_ENT,                                         KC_ENT,                         KC_LCTL,        KC_LALT,        KC_F3,          KC_F2,          KC_F1,
+        KC_LCTL,        KC_LBRC,        KC_RBRC,        KC_LALT,        KC_LCTL,                        KC_ENT,                                         KC_ENT,                         KC_LCTL,        KC_LALT,        KC_F3,          KC_F2,          KC_MPLY,
                                                                         KC_BSPC,        SH_MON,         KC_TRNS,                                        KC_TRNS,        KC_NO,          KC_SPC
     ),
     [KEYBOARD] = LAYOUT_moonlander(
@@ -100,7 +100,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRAVE,       KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_F1,                                          KC_F4,          KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_BSLS,
     KC_TAB,         KC_A,           KC_S,           KC_D,           KC_F,           KC_G,           KC_SCLN,                                        KC_MINS,        KC_H,           KC_J,           KC_K,           KC_L,           KC_SCLN,        KC_EQL,
     KC_LSFT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,                                                                           KC_N,           KC_M,           KC_COMM,        KC_DOT,         KC_SLSH,        KC_LSFT,
-    KC_MPLY,        KC_LBRC,        KC_RBRC,        KC_LALT,        KC_LCTL,                        KC_ENT,                                         KC_ENT,                         KC_LCTL,        KC_LALT,        KC_F3,          KC_F2,          KC_F1,
+    KC_LCTL,        KC_LBRC,        KC_RBRC,        KC_LALT,        KC_LCTL,                        KC_ENT,                                         KC_ENT,                         KC_LCTL,        KC_LALT,        KC_F3,          KC_F2,          KC_MPLY,
                                                                     KC_SPC,         SH_MON,         KC_TRNS,                                        KC_TRNS,        KC_NO,          KC_BSPC
     ),
 };
